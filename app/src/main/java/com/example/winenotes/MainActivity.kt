5c63d0a73c48fc47a58b1f0fe0594359488cd73e
Waraylou <waraylou@yahoo.com>
2022-04-16 16:58:46 -0500

    set up the menu items and layout

--- app/src/main/java/com/example/winenotes/MainActivity.kt
+++ app/src/main/java/com/example/winenotes/MainActivity.kt
@@ -1,18 +1,99 @@
 package com.example.winenotes
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.*
+import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.winenotes.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private var data = mutableListOf<Note>()
+    private lateinit var adapter: MyAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val layoutManager = LinearLayoutManager(this)
+        binding.myRecyclerView.setLayoutManager(layoutManager)
+
+        val divider = DividerItemDecoration(
+            applicationContext, layoutManager.orientation
+        )
+        binding.myRecyclerView.addItemDecoration(divider)
+
+        adapter = MyAdapter()
+        binding.myRecyclerView.adapter = adapter
+    }
+
+
+
+
+
+
+
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.add_menu_item) {
+
+        } else if (item.itemId == R.id.date_sort_menu_item){
+
+        } else if (item.itemId == R.id.title_sort_menu_item){
+
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+    inner class MyViewHolder(val view: View) :
+        RecyclerView.ViewHolder(view), View.OnClickListener {
+
+        init {
+            view.findViewById<View>(R.id.item_constraintLayout)
+                .setOnClickListener(this)
+        }
+
+        fun setText(text: String) {
+            view.findViewById<TextView>(R.id.item_title_textView).setText(text)
+        }
+
+        override fun onClick(view: View?) {
+            if (view != null) {
+
+            }
+        }
+    }
+
+
+    inner class MyAdapter() : RecyclerView.Adapter<MyViewHolder>() {
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+            val view =
+                LayoutInflater.from(parent.context).inflate(R.layout.item_view, parent, false)
+            return MyViewHolder(view)
+        }
+
+        override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+
+        }
+
+        override fun getItemCount(): Int {
+            return data.size
+            return 0;
+        }
 
     }
 }
